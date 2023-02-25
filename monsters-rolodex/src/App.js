eb0453d9b0679c2e61a118e6dab4002bcefcaba9
“aisse” <aisse.camara@gmail.com>
2023-02-25 15:16:33 +0100

    Hooks useEffect

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,5 @@
 // import { Component } from 'react';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
@@ -7,21 +7,30 @@ import './App.css';
 const App = () => {
 const [searchField,setSearchField] = useState('a');
 const [monsters,setMonsters] = useState([]);
+const [filteredMonsters,setFilteredMonsters] = useState(monsters);
 
-// console.log('render');
 
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response)=> response.json())
-    .then((users)=> setMonsters(users));
+useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response)=> response.json())
+      .then((users)=> setMonsters(users));
+},[]);
+
+useEffect(()=> {
+
+  const newfilteredMonsters = monsters.filter((monster) =>{
+        return  monster.name.toLocaleLowerCase().includes(searchField);
+        });
+
+        setFilteredMonsters(newfilteredMonsters);
+},[monsters, searchField]);
+
 
   const onSearchChange = (event) => {
       const searchFieldString = event.target.value.toLocaleLowerCase();
       setSearchField(searchFieldString);
     }
 
-  const filteredMonsters = monsters.filter((monster) =>{
-    return  monster.name.toLocaleLowerCase().includes(searchField);
-    });
 
   return(
     
