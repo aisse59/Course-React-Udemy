90dddcead3f1926628a0f56c932c3bf9b2407d65
“aisse” <aisse.camara@gmail.com>
2023-03-01 15:41:01 +0100

    Paint flashing

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 
 const App = () => {
 const [searchField,setSearchField] = useState('a');
+const [title,setTitle] = useState('');
 const [monsters,setMonsters] = useState([]);
 const [filteredMonsters,setFilteredMonsters] = useState(monsters);
 
@@ -25,21 +26,30 @@ useEffect(()=> {
         setFilteredMonsters(newfilteredMonsters);
 },[monsters, searchField]);
 
-
-  const onSearchChange = (event) => {
-      const searchFieldString = event.target.value.toLocaleLowerCase();
-      setSearchField(searchFieldString);
-    }
+const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+const onTitleChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setTitle(searchFieldString);
+  };
 
 
   return(
     
     <div className="App">
-      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'> {title} </h1>
       <SearchBox 
       className='monsters-search-box'
       onChangeHandler={onSearchChange} 
       placeholder='search monsters'/>
+      <br />
+      <SearchBox 
+      className='title-search-box'
+      onChangeHandler={onTitleChange} 
+      placeholder='set title'/>
+
     <CardList monsters={filteredMonsters} />
     </div>
   );
